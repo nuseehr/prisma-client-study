112ec57ebe51f18a6b95074caaf43116a044535b
Lee Sun Hong <nuseehr@naver.com>
2019-07-11 03:38:06 +0900

    add styles file

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import GlobalStyles from '../Styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import Theme from "../Styles/Theme";
-import { directive } from '@babel/types';
+
 
 export default () => (
   <ThemeProvider theme={Theme}>
