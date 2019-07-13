b10d7e22458c2d9549a11b65a9e3d8c665593312
Lee Sun Hong <nuseehr@naver.com>
2019-07-14 00:53:57 +0900

    Update lodash module

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import GlobalStyles from '../Styles/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
 import Theme from "../Styles/Theme";
+import Router from './Router';
 
 
 export default () => (
   <ThemeProvider theme={Theme}>
+  <>
     <GlobalStyles />
-    Hello
+    <Router isLoggedIn={false}/>
+  </>
   </ThemeProvider>
 );
