0d1ccad79e39f6502fe50f1ac794397f9e2634f8
Lee Sun Hong <nuseehr@naver.com>
2019-07-17 19:23:47 +0900

    Add LocalState

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -1,5 +1,9 @@
 import ApolloClient from 'apollo-boost';
 
 export default new ApolloClient({
-  uri: "http://localhost:4000"
+  uri: "http://localhost:4000",
+  clientState: {
+    defaults,
+    resolvers
+  }
 });
