7b2eacacb0d75898268e13fc7d81df8792c10c0d
Lee Sun Hong <nuseehr@naver.com>
2019-07-22 20:45:49 +0900

    Update main logo

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -2,8 +2,8 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.img`
-  width: 80%;
-  margin: 0% 10% 15%;
+  width: 70%;
+  margin: 0% 15% 15%;
 `;
 
 const Logo = ({ src }) => <Container src={src} />;
